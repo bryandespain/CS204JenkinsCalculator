cbaab2287c871a53cdf49a2cfe2e406df7d53a22
Bryan Despain <bryanjd@byu.edu>
2022-02-17 23:20:29 -0700

    Fixed uniqueId

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -76,7 +77,7 @@ class Calculator {
      */
     String createUniqueID(String n){
 
-        return null;
+        return n + UUID.randomUUID();
     }
 
 
