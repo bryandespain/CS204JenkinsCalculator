4c07af5aa6923db7619d4a70f1ec199c49e06b28
Bryan Despain <bryanjd@byu.edu>
2022-02-20 16:51:35 -0700

    new stuff

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("Enter ");
+  }
+}
